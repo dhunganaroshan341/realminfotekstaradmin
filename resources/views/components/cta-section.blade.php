d80fed39798b95af31ec8fd4b6ca0fbf6128e77e
dhunganaroshan341 <dhunganaroshan341@gmail.com>
2025-07-13 23:08:11 +0545

    Add changes

--- resources/views/components/cta-section.blade.php
+++ resources/views/components/cta-section.blade.php
@@ -1,25 +1,26 @@
 @php
     $backgroundImage =
         isset($cta) && $cta->image ? asset('uploads/' . $cta->image) : asset('assets/images/hero_cta.jpg');
-
     $ctaTitle = $cta->title ?? 'Do you need help?';
     $ctaDescription =
         $cta->description ?? 'We are here to assist you with your queries and problems. Reach out to us now!';
 @endphp
 
-<section class="section-4 py-5 text-center"
-    style="background-image: url('{{ $backgroundImage }}'); background-size: cover; background-position: center; position: relative;">
+<section class="section-4 py-5 text-center text-white position-relative"
+    style="background-image: url('{{ $backgroundImage }}');
+                background-size: cover;
+                background-position: center;">
 
-    <div class="hero-background-overlay"
-        style="position: absolute; top: 0; left: 0; width: 100%; height: 100%; background-color: rgba(0,0,0,0.4); z-index: 1;">
-    </div>
+    {{-- Overlay --}}
+    <div class="position-absolute top-0 start-0 w-100 h-100" style="background-color: rgba(0,0,0,0.5); z-index: 1;"></div>
 
+    {{-- Content --}}
     <div class="container position-relative z-2">
         <div class="row justify-content-center">
             <div class="col-12 col-md-10 col-lg-8">
-                <div class="help-container text-white">
-                    <h1 class="title">{{ $ctaTitle }}</h1>
-                    <p class="card-text mt-3">{!! $ctaDescription !!}</p>
+                <div class="help-container py-4">
+                    <h1 class="title mb-3">{{ $ctaTitle }}</h1>
+                    <p class="lead">{!! $ctaDescription !!}</p>
                     <a href="{{ url('/contact-us') }}" class="btn btn-primary mt-3">
                         Reach Out <i class="fa-solid fa-angle-right"></i>
                     </a>
