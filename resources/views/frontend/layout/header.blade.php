cc759a7e44ae13c287ecbfda4ee5ecc03bffbbf7
dhunganaroshan341 <dhunganaroshan341@gmail.com>
2025-07-13 17:18:21 +0545

    Add changes

--- resources/views/frontend/layout/header.blade.php
+++ resources/views/frontend/layout/header.blade.php
@@ -38,7 +38,12 @@
 
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarmain"
                 aria-controls="navbarmain" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                    aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon">
+                        <i class="fas fa-bars"></i>
+                    </span>
+                </button>
             </button>
 
             <div class="collapse navbar-collapse" id="navbarmain">
