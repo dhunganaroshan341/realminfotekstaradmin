760025ca547920688e7c8609b4c81994efe91954
dhunganaroshan341 <dhunganaroshan341@gmail.com>
2025-07-13 17:39:26 +0545

    Add changes

--- app/Models/Setting.php
+++ app/Models/Setting.php
@@ -8,5 +8,5 @@ use Illuminate\Database\Eloquent\Model;
 class Setting extends Model
 {
     use HasFactory;
-    protected $fillable=['logo','title','email','address','contact','description','work_description','facebook_url','twitter_url','github_url','instagram_url','welcome_image','about_image'];
+    protected $fillable=['logo','title','email','address','contact','contact_2','description','work_description','facebook_url','twitter_url','github_url','instagram_url','welcome_image','about_image'];
 }
