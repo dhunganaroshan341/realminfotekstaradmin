087cf57b9fd22577169011b2644e5e46707e926b
dhunganaroshan341 <dhunganaroshan341@gmail.com>
2025-07-14 09:15:04 +0545

    Add changes

--- public/js/admin/client/client.js
+++ public/js/admin/client/client.js
@@ -4,6 +4,17 @@ $(document).ready(function () {
         height: 300,
     });
 
+    let currentAction = "";
+
+$(document).on("click", ".submitBtn", function () {
+    currentAction = "store";
+});
+
+$(document).on("click", ".updateBtn", function () {
+    currentAction = "update";
+});
+
+
     // Data Table
     var table = $("#show-client-data").DataTable({
         processing: true,
@@ -113,7 +124,7 @@ $(document).ready(function () {
                     }
                 },
                 complete: function () {
-                    $(".submitBtn").prop("disabled", false);
+                    // $(".submitBtn").prop("disabled", false);
                 },
             });
         }
