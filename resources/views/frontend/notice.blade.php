ecdf20f0cde751b294ad2e82ced949ada205f131
dhunganaroshan341 <dhunganaroshan341@gmail.com>
2025-07-14 17:24:54 +0545

    Add changes

--- resources/views/frontend/notice.blade.php
+++ resources/views/frontend/notice.blade.php
@@ -1,4 +1,3 @@
-<!-- Modal -->
 @if (isset($notice) && $notice != null)
     <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1"
         aria-labelledby="staticBackdropLabel" aria-hidden="true">
@@ -8,15 +7,24 @@
                     <h5 class="modal-title" id="staticBackdropLabel">{{ $notice->title ?? 'Notice' }}</h5>
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
-                <div class="modal-body">
-                    <div class="card" style="width: 100%;">
-                        <img src="{{ asset('uploads/' . $notice->image) }}" class="card-img-top" alt="...">
+
+                <div class="modal-body p-0">
+                    @if (!empty($notice->url))
+                        <a href="{{ $notice->url }}" target="_blank" class="text-decoration-none text-dark d-block">
+                    @endif
+
+                    <div class="card border-0" style="width: 100%;">
+                        <img src="{{ asset('uploads/' . $notice->image) }}" class="card-img-top" alt="Notice Image">
                         <div class="card-body">
-                            {{-- <h5 class="card-title">{{ $notice->title }}</h5> --}}
                             <p class="card-text">{!! $notice->description !!}</p>
                         </div>
                     </div>
+
+                    @if (!empty($notice->url))
+                        </a>
+                    @endif
                 </div>
+
                 <div class="modal-footer">
                     <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
                 </div>
