6c4f21cb1a1220c9fbb209165c5f5ddc55118391
dhunganaroshan341 <dhunganaroshan341@gmail.com>
2025-07-16 07:25:29 +0545

    Add changes

--- resources/views/Admin/Button/button.blade.php
+++ resources/views/Admin/Button/button.blade.php
@@ -1,29 +1,30 @@
-@if (Route::currentRouteName() == 'admin.user')
-    <button title="change password" type="button"
-        class="btn p-0 m-0 mx-1 bg-transparent border-0 text-info resetUserBtn {{ $data->role == 'User' ? 'd-none' : 'd-block' }}"
-        data-id="{{ $data->id }}">
-        <i class="fas fa-lock"></i>
-    </button>
+@if (Route::currentRouteName() === 'admin.user')
+    @if ($data->role !== 'User')
+        <!-- Show all buttons for roles other than 'User' -->
+        <button title="Change Password" type="button"
+            class="btn p-0 m-0 mx-1 bg-transparent border-0 text-info resetUserBtn" data-id="{{ $data->id }}">
+            <i class="fas fa-lock"></i>
+        </button>
 
-    <button title="edit" type="button"
-        class="btn p-0 m-0 mx-1 bg-transparent border-0 text-warning editUserButton {{ $data->role == 'User' ? 'd-none' : 'd-block' }}"
-        data-action="edit" data-id="{{ $data->id }}">
-        <i class="fas fa-pencil"></i>
-    </button>
+        <button title="Edit" type="button" class="btn p-0 m-0 mx-1 bg-transparent border-0 text-warning editUserButton"
+            data-action="edit" data-id="{{ $data->id }}">
+            <i class="fas fa-pencil-alt"></i>
+        </button>
 
-    <button title="delete" type="button"
-        class="btn p-0 m-0 mx-1 bg-transparent border-0 text-danger deleteData {{ $data->role == 'User' ? 'd-none' : 'd-block' }}"
-        data-id="{{ $data->id }}">
-        <i class="fas fa-trash"></i>
-    </button>
+        <button title="Delete" type="button" class="btn p-0 m-0 mx-1 bg-transparent border-0 text-danger deleteData"
+            data-id="{{ $data->id }}">
+            <i class="fas fa-trash-alt"></i>
+        </button>
+    @endif
 @else
-    <button title="edit" type="button" class="btn p-0 m-0 mx-1 bg-transparent border-0 text-warning editUserButton"
+    <!-- Default buttons for all roles on other routes -->
+    <button title="Edit" type="button" class="btn p-0 m-0 mx-1 bg-transparent border-0 text-warning editUserButton"
         data-id="{{ $data->id }}">
-        <i class="fas fa-pencil"></i>
+        <i class="fas fa-pencil-alt"></i>
     </button>
 
-    <button title="delete" type="button" class="btn p-0 m-0 mx-1 bg-transparent border-0 text-danger deleteData"
+    <button title="Delete" type="button" class="btn p-0 m-0 mx-1 bg-transparent border-0 text-danger deleteData"
         data-id="{{ $data->id }}">
-        <i class="fas fa-trash"></i>
+        <i class="fas fa-trash-alt"></i>
     </button>
 @endif
