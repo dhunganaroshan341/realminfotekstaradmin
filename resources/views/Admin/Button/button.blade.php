d7e8efe006ecdd4b67511e1a5a50b15975939643
dhunganaroshan341 <dhunganaroshan341@gmail.com>
2025-07-16 07:27:44 +0545

    Add changes

--- resources/views/Admin/Button/button.blade.php
+++ resources/views/Admin/Button/button.blade.php
@@ -1,25 +1,16 @@
-@if (Route::currentRouteName() === 'admin.user')
-    @if ($data->role !== 'User')
-        <!-- Show all buttons for roles other than 'User' -->
-        <button title="Change Password" type="button"
-            class="btn p-0 m-0 mx-1 bg-transparent border-0 text-info resetUserBtn" data-id="{{ $data->id }}">
-            <i class="fas fa-lock"></i>
-        </button>
+@php
+    $isAdmin = auth()->check() && auth()->user()->role === 'Admin';
+@endphp
 
-        <button title="Edit" type="button" class="btn p-0 m-0 mx-1 bg-transparent border-0 text-warning editUserButton"
-            data-action="edit" data-id="{{ $data->id }}">
-            <i class="fas fa-pencil-alt"></i>
-        </button>
-
-        <button title="Delete" type="button" class="btn p-0 m-0 mx-1 bg-transparent border-0 text-danger deleteData"
-            data-id="{{ $data->id }}">
-            <i class="fas fa-trash-alt"></i>
-        </button>
-    @endif
-@else
-    <!-- Default buttons for all roles on other routes -->
-    <button title="Edit" type="button" class="btn p-0 m-0 mx-1 bg-transparent border-0 text-warning editUserButton"
+@if ($isAdmin)
+    <!-- Show to logged-in admin only -->
+    <button title="Change Password" type="button" class="btn p-0 m-0 mx-1 bg-transparent border-0 text-info resetUserBtn"
         data-id="{{ $data->id }}">
+        <i class="fas fa-lock"></i>
+    </button>
+
+    <button title="Edit" type="button" class="btn p-0 m-0 mx-1 bg-transparent border-0 text-warning editUserButton"
+        data-action="edit" data-id="{{ $data->id }}">
         <i class="fas fa-pencil-alt"></i>
     </button>
 
