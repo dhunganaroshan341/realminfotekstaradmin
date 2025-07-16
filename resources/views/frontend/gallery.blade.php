3966310b68edc7b2a699a23f6ceb54f77f7752c8
dhunganaroshan341 <dhunganaroshan341@gmail.com>
2025-07-16 11:49:23 +0545

    Add changes

--- resources/views/frontend/gallery.blade.php
+++ resources/views/frontend/gallery.blade.php
@@ -231,9 +231,9 @@
                 <div class="card-body d-flex justify-content-between align-items-center">
                     <h5 class="card-title mb-0">${album.title}</h5>
                     ${hasMedia ? `
-                                                                                                                        <a href="${downloadLink}" class="btn btn-sm btn-outline-primary" download title="Download PDF">
-                                                                                                                            <i class="fas fa-download"></i>
-                                                                                                                        </a>` : ''}
+                                                                                                                                        <a href="${downloadLink}" class="btn btn-sm btn-outline-primary" download title="Download PDF">
+                                                                                                                                            <i class="fas fa-download"></i>
+                                                                                                                                        </a>` : ''}
                 </div>
             </div>
         </div>
@@ -424,6 +424,10 @@
 
             #mobileSidebar {
                 background: #292771c9;
+            }
+
+            .list-group-item {
+                background: #ffffff91;
             }
         </style>
     @endpush
