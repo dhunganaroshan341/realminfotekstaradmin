9d0e9aa339dbe62f990b078e6f0aed798acc4927
dhunganaroshan341 <dhunganaroshan341@gmail.com>
2025-07-16 10:59:40 +0545

    Add changes

--- public/js/admin/client/tags.js
+++ public/js/admin/client/tags.js
@@ -1,39 +1,39 @@
-$(document).ready(function () {
-$(function () {
-        let tags = [];
+// $(document).ready(function () {
+// $(function () {
+//         let tags = [];
 
-        function renderTags() {
-            $('#tagContainer').empty();
-            tags.forEach((tag, index) => {
-                $('#tagContainer').append(`
-                    <span class="tag">
-                        ${tag}
-                        <span class="remove-tag ms-1" data-index="${index}">&times;</span>
-                    </span>
-                `);
-            });
-            $('#post_tags').val(tags.join(','));
-        }
+//         function renderTags() {
+//             $('#tagContainer').empty();
+//             tags.forEach((tag, index) => {
+//                 $('#tagContainer').append(`
+//                     <span class="tag">
+//                         ${tag}
+//                         <span class="remove-tag ms-1" data-index="${index}">&times;</span>
+//                     </span>
+//                 `);
+//             });
+//             $('#post_tags').val(tags.join(','));
+//         }
 
-        $('#addTagButton').on('click', function () {
-            const value = $('#tagInput').val().trim();
-            if (value !== '' && !tags.includes(value)) {
-                tags.push(value);
-                renderTags();
-                $('#tagInput').val('');
-            }
-        });
+//         $('#addTagButton').on('click', function () {
+//             const value = $('#tagInput').val().trim();
+//             if (value !== '' && !tags.includes(value)) {
+//                 tags.push(value);
+//                 renderTags();
+//                 $('#tagInput').val('');
+//             }
+//         });
 
-        $(document).on('click', '.remove-tag', function () {
-            const index = $(this).data('index');
-            tags.splice(index, 1);
-            renderTags();
-        });
+//         $(document).on('click', '.remove-tag', function () {
+//             const index = $(this).data('index');
+//             tags.splice(index, 1);
+//             renderTags();
+//         });
 
-        $('#formModal').on('hidden.bs.modal', function () {
-            tags = [];
-            renderTags();
-            $('#tagInput').val('');
-        });
-    });
-});
+//         $('#formModal').on('hidden.bs.modal', function () {
+//             tags = [];
+//             renderTags();
+//             $('#tagInput').val('');
+//         });
+//     });
+// });
