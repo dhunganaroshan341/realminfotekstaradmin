76b1c948f655f0a89ee19a40fe1a98a3ab40c9c7
dhunganaroshan341 <dhunganaroshan341@gmail.com>
2025-07-16 07:23:29 +0545

    Add changes

--- resources/views/Admin/Button/button.blade.php
+++ resources/views/Admin/Button/button.blade.php
@@ -1,30 +1,28 @@
 @if (Route::currentRouteName() == 'admin.user')
     <button title="change password" type="button"
-        class="btn p-0 m-0 mx-1 bg-transparent border-0 text-info resetUserBtn {{ $data->role=='User' ? 'd-none':'' }}"
+        class="btn p-0 m-0 mx-1 bg-transparent border-0 text-info resetUserBtn {{ $data->role == 'User' ? 'd-none' : 'd-block' }}"
         data-id="{{ $data->id }}">
-       <i class="fas fa-lock"></i>
+        <i class="fas fa-lock"></i>
     </button>
 
     <button title="edit" type="button"
-        class="btn p-0 m-0 mx-1 bg-transparent border-0 text-warning editUserButton {{ $data->role=='User' ? 'd-none':'' }}"
+        class="btn p-0 m-0 mx-1 bg-transparent border-0 text-warning editUserButton {{ $data->role == 'User' ? 'd-none' : 'd-block' }}"
         data-action="edit" data-id="{{ $data->id }}">
         <i class="fas fa-pencil"></i>
     </button>
 
     <button title="delete" type="button"
-        class="btn p-0 m-0 mx-1 bg-transparent border-0 text-danger deleteData {{ $data->role=='User' ? 'd-none':'' }}"
+        class="btn p-0 m-0 mx-1 bg-transparent border-0 text-danger deleteData {{ $data->role == 'User' ? 'd-none' : 'd-block' }}"
         data-id="{{ $data->id }}">
         <i class="fas fa-trash"></i>
     </button>
 @else
-    <button title="edit" type="button"
-        class="btn p-0 m-0 mx-1 bg-transparent border-0 text-warning editUserButton"
+    <button title="edit" type="button" class="btn p-0 m-0 mx-1 bg-transparent border-0 text-warning editUserButton"
         data-id="{{ $data->id }}">
         <i class="fas fa-pencil"></i>
     </button>
 
-    <button title="delete" type="button"
-        class="btn p-0 m-0 mx-1 bg-transparent border-0 text-danger deleteData"
+    <button title="delete" type="button" class="btn p-0 m-0 mx-1 bg-transparent border-0 text-danger deleteData"
         data-id="{{ $data->id }}">
         <i class="fas fa-trash"></i>
     </button>
